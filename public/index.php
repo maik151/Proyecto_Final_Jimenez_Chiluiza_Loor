2c84c79f13911dc58f547427cbd09043eb166437
maicol_jb <64624079+maik151@users.noreply.github.com>
2025-02-20 23:14:17 -0500

    - Validacion de que los cambios se suben arepositorio remoto en github

--- public/index.php
+++ public/index.php
@@ -9,3 +9,38 @@
     <div>   LIBROS Y AUTORES :)</div>
 </body>
 </html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>   LIBROS Y AUTORES :)</div>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>   LIBROS Y AUTORES :)</div>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>   LIBROS Y AUTORES :)</div>
+</body>
+</html>
